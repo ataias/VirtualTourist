69f7bfbcede89f161a7196a50769d1623adbdcf8
Ataias Pereira Reis <ataiasreis@gmail.com>
2021-02-13 07:34:20 -0300

    Initial Commit

---
+++ VirtualTourist/VirtualTouristApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualTouristApp.swift
+//  VirtualTourist
+//
+//  Created by Ataias Pereira Reis on 13/02/21.
+//
+
+import SwiftUI
+
+@main
+struct VirtualTouristApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
